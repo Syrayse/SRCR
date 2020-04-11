bc4476d76d024243457973b89d0037dae6f30832
syrayse <ruipedronetoreis12@gmail.com>
2020-04-11 20:49:23 +0000

    Finalizadas as operações de contrato

--- src/contrato.pl
+++ src/contrato.pl
@@ -16,44 +16,72 @@
 % Dado um ID, coloca em R todos os contratos com esse ID.
 
 getContratoID(ID,R) :-
-	...
+	solucoes(
+		(IdAdte,IdAdria),
+		(contrato(ID,IdAdte,IdAdria,_,_,_,_,_,_,_)),
+		R).
 
 % Dado um contrato, coloca em R contratos desse tipo.
 
 getContratoPorTipo(Tipo,R) :-
-	...
+	solucoes(
+		ID,
+		(contrato(ID,_,_,Tipo,_,_,_,_,_,_)),
+		R).
 
 % Predicado que dado um tipo de procedimento, coloca em R
 % todos os contratos deste tipo com um valor superior a Valor.
 
 getContratoPorProcedimento(Proc,Valor,R) :-
-	...
+	solucoes(
+		ID,
+		(contrato(ID,_,_,_,Proc,_,Va,_,_,_),
+			Va > Valor),
+		R).
 
 % Predicado que dado um tipo de contrato e um tipo de proc.
 % coloca em R todos os contratos deste genero, com custo superior a Valor.
 
 getContratoPorTipoEProcedimento(Tipo,Proc,Valor,R) :-
-	...
+	solucoes(
+		ID,
+		(contrato(ID,_,_,Tipo,Proc,_,Va,_,_,_),
+			Va > Valor),
+		R).
 
 % Coloca em R todos os contratos relativos ao adjudicante indicado.
 
 getContratoPorAdjudicante(NIF,R) :-
-	...
+	solucoes(
+		ID,
+		(contrato(ID,NIF,_,_,_,_,_,_,_,_)),
+		R).
 
 % Coloca em R todos os contratos relativos ao adjudicatario indicado.
 
 getContratoPorAdjudicatario(NIF,R) :-
-	...
+	solucoes(
+		ID,
+		(contrato(ID,_,NIF,_,_,_,_,_,_,_)),
+		R).
 
 % Coloca em R todos os contratos relativos a um adjudicante, segundo o seu nome.
 
 getContratoPorNome(Nome,R) :-
-	...
+	solucoes(
+		ID,
+		(contrato(ID,_,NIF,_,_,_,_,_,_,_),
+			adjudicante(NIF,Nome,_)),
+		R).
 
 % Coloca em R os contratos feito por todos o adjudicantes que não possuam o nome indicado.
 
 getContratoExcetoNome(Nome,R) :-
-	...
+	solucoes(
+		ID,
+		(contrato(ID,_,NIF,_,_,_,_,_,_,_),
+			-adjudicante(NIF,Nome,_)),
+		R).
 
 % Adiciona um novo contrato
 
