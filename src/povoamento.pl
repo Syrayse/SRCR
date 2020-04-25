ca87e8c3b830409ca5a92ccb47e45319f7379707
syrayse <ruipedronetoreis12@gmail.com>
2020-04-25 14:00:52 +0100

    adicionado ficheiro de povoamento (falta preencher).

---
+++ src/povoamento.pl
@@ -0,0 +1,85 @@
+% #########################################################
+% ## SRCR - Dominio de Contratacao Publica.              ##
+% ## Desenvolvido pelo grupo de trabalho nº 10.          ##
+% ## Ano letivo 2019/20 - MIEI.                          ##
+% ##                                                     ##
+% ## --------------------------------------------------- ##
+% ##                                                     ##
+% ## Ficheiro: povoamento.pl                             ##
+% ## Descricao: O presente ficheiro  tem o  objetivo  de ##
+% ## definir fazer um povoamento  inicial da base de co- ##
+% ## nhecimento para esta, e  respectivos predicado, po- ##
+% ## derem posteriormente ser testados. Fazendo a inser- ##
+% ## cao de conhecimento  perfeito e imperfeito adequado ##
+% ## ao seu respetivo teste.                             ##
+% #########################################################
+
+% #########################################################
+% Povoamento de conhecimento perfeito.
+
+% 1) Pessoa.
+
+
+% 2) Empresa.
+
+
+% 3) Crime.
+
+
+% 4) Interdito.
+
+
+% 5) Inabilitado.
+
+
+% 6) Administrador.
+
+
+% 7) Fiscal.
+
+
+% 7.1) Fiscaliza.
+
+
+% 8) Sub-empresa.
+
+
+% 9) Contrato.
+
+
+% #########################################################
+
+% #########################################################
+% Povoamento de conhecimento imperfeito.
+
+% 1) Pessoa.
+
+
+% 2) Empresa.
+
+
+% 3) Crime.
+
+
+% 4) Interdito.
+
+
+% 5) Inabilitado.
+
+
+% 6) Administrador.
+
+
+% 7) Fiscal.
+
+
+% 7.1) Fiscaliza.
+
+
+% 8) Sub-empresa.
+
+
+% 9) Contrato.
+
+
+% #########################################################
